db4243113ff58a531ee5c98114a9dd1c86435406
youjin <yjinhann@gmail.com>
2023-06-15 11:47:36 +0900

    add: countUniqueValues

---
+++ multiple-pointers/practice-countUniqueValues.js
@@ -0,0 +1,20 @@
+// Count Unique Values
+// Implement a function called countUniqueValues,
+// which accepts a sorted Array, and counts the unique values in the Array.
+// There can be negative numbers in the Array, but it will always be sorted.
+
+const countUniqueValues = (arr) => {
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+};
+
+console.log(countUniqueValues([1, 1, 1, 1, 1, 2])); //2
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); //7
+countUniqueValues([]); //0
+countUniqueValues([-2, -1, -1, 0, 1]); //2
